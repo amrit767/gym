91f35a18d0538261f333cbf5e2918d8fdc1b93ed
amrit sood <soodamrit90@gmail.com>
2026-05-07 17:55:27 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const mylabel = document.getElementById("mylabel"); //weight
+const mylabel = document.getElementById("mylabel"); 
 const myinput = document.getElementById("myinput");
 
-const mylabel2 = document.getElementById("mylabel2"); //height
+const mylabel2 = document.getElementById("mylabel2"); 
 const myinput2 = document.getElementById("myinput2");
 
-const mysubumit = document.getElementById("mysubmit"); //submitbutton
+const mysubumit = document.getElementById("mysubmit"); 
 
-const bmilabel = document.getElementById("bmilabel"); //result
+const bmilabel = document.getElementById("bmilabel"); 
+
+const bmiwru = document.getElementById("bmiwru"); 
 
 let bmi;
 
@@ -17,5 +19,22 @@ mysubumit.onclick = function(){
     bmi = weight / (height ** 2);
 
     bmilabel.textContent = bmi;
+
+
+ if(bmi <= 18.5){
+    bmilabel.textContent = 'you are underweight';
 }
- 
+
+else if(bmi <= 24.9){
+    bmilabel.textContent = 'you are normal'
+}
+
+else if(bmi <= 29.9){
+    bmilabel.textContent = 'you are overweight'
+}
+
+else{
+    bmilabel.textContent = 'you are obese';
+}
+
+}
